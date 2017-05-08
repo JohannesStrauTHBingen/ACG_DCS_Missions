610e31210111d7e13fc401355397e4570eb0a57e
JohannesStrauTHBingen <johannes.a.strauss@th-bingen.de>
2017-05-08 21:40:26 +0200

    fix

--- PhoneBoothMission.lua
+++ PhoneBoothMission.lua
@@ -1,3 +1,4 @@
+
 -- 6 Gruppen 3 pro Seite
 -- Eine gruppe umzingelt Airfield (pro Seite)
 -- Nach Zerstörung dieser Gruppe airfield -> forward operationg base
@@ -10,7 +11,7 @@
 -- Einheiten befreit wurde und eigene Einheiten drin stehen
 -- Umsetzung: mit Zone.
 
-redClientSet = SET_CLIENT:FilterCoalitions("blue")
+blueClientSet = SET_CLIENT:FilterCoalitions("blue")
   :FilterStart()
   :ForEachClientInZone(ZONE:New("WarningZoneBlue"),
     function (clientobject)
@@ -24,10 +25,10 @@ redClientSet = SET_CLIENT:FilterCoalitions("red")
   :FilterStart()
   :ForEachClientInZone(ZONE:New("WarningZoneRed"),
     function (client)
-      MessageToUnit(client:GetClientGroupDCSUnit(), "You're to close to the enemy Base. Return to CombatZone if you don't want to blow up.",20, CLIENT:GetClientGroupName() + " Close at enemy Base." )
+      client:E({MessageToUnit(client:GetClientGroupDCSUnit(), "You're to close to the enemy Base. Return to CombatZone if you don't want to blow up.",20, CLIENT:GetClientGroupName() + " Close at enemy Base." )})
     end)
   :ForEachClientInZone(ZONE:New("spawnProtectionZoneRed"),function(client)
-    client:GetDCSGroup():Destroy()
+    client:E({client:GetDCSGroup():Destroy()})
   end)
 
 -- beslanGroup is the Group that is holding the Airfield Beslan. If it gets destryoed a event is triggered
@@ -39,13 +40,25 @@ beslanGroup:HandleEvent(EVENTS.Dead, beslanLibarated())
 function beslanLibarated()
   beslanGroupSet = SET_GROUP:FilterPrefixes("Red_Beslan")
     :FilterStart():ForEachGroup(function(group)
-    group:Activate()
-    end
-    )
-
+    group:E({group:Activate()})
+    end)
   MessageToAll("Red Team has managed to free Beslan! They can spawn there now.", 60 ,"Free Beslan!")
   spawnNewGroup("Garrison_Beslan")
   spawnNewGroup("Attacking_Forces_Blue_Mozdok")
+end
+
+nalchikGroup = GROUP:FindByName("Attack_Force_BLUE")
+nalchikGroup:HandleEvent(EVENTS.Dead, nalchikLiberated())
+
+function nalchikLiberated()
+  nalchikGroupSet = SET_GROUP:FilterPrefixes("Blue_Nalchik")
+    :FilterStart()
+    :ForEachGroup(function(group)
+      group:E({group:Activate()})
+    end)
+  MessageToAll("Blue Team has managed to free Nalchik! They can spawn there now.", 60 ,"Free Nalchik!")
+  spawnNewGroup("Garrison_Nalchik")
+  spawnNewGroup("Attacking_Forces_Red_Vody")
 end
 
 -- Spawned eine Gruppe mit ihr übergebenden namen.

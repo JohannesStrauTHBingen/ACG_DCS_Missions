ebfeb7ed8532431078a23b8509b5d591512018c2
JohannesStrauTHBingen <johannes.a.strauss@th-bingen.de>
2017-05-08 19:53:00 +0200

    kleines Update

--- PhoneBoothMission.lua
+++ PhoneBoothMission.lua
@@ -14,28 +14,21 @@
 redClientSet = SET_CLIENT:FilterCoalitions("blue")
   :FilterStart()
   :ForEachClientInZone(ZONE:New("WarningZoneBlue"),
-  function (clientobject)
-  	MessageToUnit(clientobject:GetClientGroupDCSUnit(), "You're to close to the enemy Base. Return to CombatZone if you don't want to blow up.",20, CLIENT:GetClientGroupName() + " Close at enemy Base." )
+    function (clientobject)
+      MessageToUnit(clientobject:GetClientGroupDCSUnit(), "You're to close to the enemy Base. Return to CombatZone if you don't want to blow up.",20, CLIENT:GetClientGroupName() + " Close at enemy Base." )
+    end)
+  :ForEachClientInZone(ZONE:New("spawnProtectionZoneBlue"),function(client)
+    client:GetDCSGroup():Destroy()
   end)
 
 redClientSet = SET_CLIENT:FilterCoalitions("red")
   :FilterStart()
   :ForEachClientInZone(ZONE:New("WarningZoneRed"),
-  function (clientobject)
-    MessageToUnit(clientobject:GetClientGroupDCSUnit(), "You're to close to the enemy Base. Return to CombatZone if you don't want to blow up.",20, CLIENT:GetClientGroupName() + " Close at enemy Base." )
-  end)
-
-redClientGroupSet = SET_GROUP:FilterPrefixes("Blue_")
-  :FilterStart()
-  :ForEachGroupCompletelyInZone(ZONE:New("SpawnProtectionZoneRed"),
-    function(groupObject)
-      groupObject:Destroy()
+    function (client)
+      MessageToUnit(client:GetClientGroupDCSUnit(), "You're to close to the enemy Base. Return to CombatZone if you don't want to blow up.",20, CLIENT:GetClientGroupName() + " Close at enemy Base." )
     end)
-
-blueClientGroupSet = SET_GROUP:FilterPrefixes("Red_")
-  :FilterStart()
-  :ForEachGroupCompletelyInZone(ZONE:New("SpawnProtectionZoneBlue"),function(groupObject)
-    groupObject:Destroy()
+  :ForEachClientInZone(ZONE:New("spawnProtectionZoneRed"),function(client)
+    client:GetDCSGroup():Destroy()
   end)
 
 -- beslanGroup is the Group that is holding the Airfield Beslan. If it gets destryoed a event is triggered
@@ -46,11 +39,11 @@ beslanGroup:HandleEvent(EVENTS.Dead, beslanLibarated())
 -- It will only handle function calls!!
 function beslanLibarated()
   beslanGroupSet = SET_GROUP:FilterPrefixes("Red_Beslan")
-  :FilterStart():ForEachGroup(function(group)
-  group:Activate()  
-  end
-  )
-  
+    :FilterStart():ForEachGroup(function(group)
+    group:Activate()
+    end
+    )
+
   MessageToAll(message, 60 ,"Red Team has managed to free Beslan! They can spawn there now")
   spawnNewGroup("Garrison_Beslan")
   spawnNewGroup("Attacking_Forces_Blue_Mozdok")

c9bc14865c9ba9dd04cb8a26902588561d79154c
Johannes <johannes.a.strauss@th-bingen.de>
2017-05-22 16:33:10 +0200

    Fix basierend auf der Antwort im MOOSE Slack

--- PhoneBoothMission.lua
+++ PhoneBoothMission.lua
@@ -1,21 +1,29 @@
 
 
 --Die Gruppe die Beslan hält
-local beslanOccupationGroup = GROUP:FindByName("Attacking_Forces_Blue")
-beslanOccupationGroup.HandleEvent(EVENTS.Dead, beslanLibarated())
+beslanOccupationGroup = GROUP:FindByName("Attacking_Forces_Blue")
+beslanOccupationGroup.HandleEvent(EVENTS.Dead)
+function beslanOccupationGroup:OnEventDead(EventData)
+  if not EventData.IniGroup:IsAlive() then
+    beslanLibarated()
+  end
+end
+
+beslanDefense = SPAWN:New("Beslan_Defense")
+newBlueUnits = SPAWN:New("New_Blue_Units")
+beslanMessage = MESSAGE:New("Beslan got liberated by Red Forces. It can now serve as secondary Aifrield for Red!",30,nil)
 
 function beslanLibarated()
-  local beslanMessage = MESSAGE:New("Beslan got liberated by Red Forces. It can now serve as secondary Aifrield for Red!",30,nil)
   beslanMessage:ToAll()
-  
-  --Spawn Airfield Verteidigung
-  local beslanDefense = SPAWN:New("Beslan_Defense")
-  beslanDefense:Spawn()
-
-  --Spawn Neue blaue Angriffs Gruppe
-  local newBlueUnits = SPAWN:New("New_Blue_Units")
+  --Spawn Airfield Defense
+  beslanDefenseGroup = beslanDefense:Spawn()
+  --Spawn new Blue Attack forces
   newBlueUnits:Spawn()
 end
+
+
+
+
 
 
 

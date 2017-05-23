55eae364613b4c0c0d016173958f7dc80e22aa47
Johannes <johannes.a.strauss@th-bingen.de>
2017-05-23 15:37:11 +0200

    Debug version

--- PhoneBoothMission.lua
+++ PhoneBoothMission.lua
@@ -1,33 +1,24 @@
 
-
 --Die Gruppe die Beslan hält
-
-
-beslanDefense = SPAWN:New("Garrison_Beslan")
-beslanMessage = MESSAGE:New("Beslan got liberated by Red Forces. It can now serve as secondary Aifrield for Red!",30,nil)
-newBlueUnits = SPAWN:New("Attacking_Forces_Blue_Mozdok")
-
 beslanOccupationGroup = GROUP:FindByName("Attacking_Forces_Blue")
 beslanOccupationGroup.HandleEvent(EVENTS.Dead)
-
 function beslanOccupationGroup:OnEventDead(EventData)
+  
+  MESSAGE:New("OnEventDead by beslanOccuation Group got triggert!",30,nil):ToAll()
+  
   if not EventData.IniGroup:IsAlive() then
     beslanLibarated()
   end
 end
 
+beslanDefense = SPAWN:New("Beslan_Defense")
+newBlueUnits = SPAWN:New("New_Blue_Units")
+beslanMessage = MESSAGE:New("Beslan got liberated by Red Forces. It can now serve as secondary Aifrield for Red!",30,nil)
+
 function beslanLibarated()
   beslanMessage:ToAll()
-  beslanDefense:Spawn()
+  --Spawn Airfield Defense
+  beslanDefenseGroup = beslanDefense:Spawn()
+  --Spawn new Blue Attack forces
   newBlueUnits:Spawn()
 end
-
-
-
-
-
-
-
-
-
-

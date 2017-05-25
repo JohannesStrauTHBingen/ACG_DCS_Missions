1a08434436a6dd2f4b13c8de9b332c82ca9bd640
Johannes <johannes.a.strauss@th-bingen.de>
2017-05-25 14:47:03 +0200

    Calais Bomber Mission

---
+++ DCS_Missions/src/CalaisBombing.lua
@@ -0,0 +1,14 @@
+
+
+--Spawn a BomberGroup every 30 minutes
+
+GROUP:GetMaxHeight()
+
+bomberSpawn = SPAWN:New( "Bombers" )
+  :SpawnScheduled(1800,0)  
+  :OnSpawnGroup(function( SpawnGoup )
+  
+  MESSAGE:New("Bombers are on their way to Calais at " .. SpawnGroup:GetMaxHeight() ..  " m! Protect them." ,200,nil):ToBlue()
+  MESSAGE:New("Möbelwagen detected on their way to Calais " .. SpawnGroup:GetMaxHeight() ..  " m! Pauke Pauke!." ,200,nil):ToRed()
+  
+  end)

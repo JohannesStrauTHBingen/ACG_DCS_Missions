1701815a4c510abc6aec52435b1c489768a1a3f3
Johannes <johannes.a.strauss@th-bingen.de>
2017-05-27 12:08:38 +0200

    changes

--- DCS_Missions/src/CalaisBombing.lua
+++ DCS_Missions/src/CalaisBombing.lua
@@ -4,17 +4,25 @@
 bomberSpawn = SPAWN:New( "Bombers" )
   :SpawnScheduled(1800,0)
   :OnSpawnGroup(function( SpawnGoup )
-      MESSAGE:New("Bombers are on their way to ! Protect them." ,200,nil):ToBlue()
-      MESSAGE:New("Möbelwagen detected on their way ! Pauke Pauke!." ,200,nil):ToRed()
+    MESSAGE:New("Bombers are on their way to ! Protect them." ,200,nil):ToBlue()
+    MESSAGE:New("Möbelwagen detected on their way ! Pauke Pauke!." ,200,nil):ToRed()
 
   end)
 
 radarSchedular = SCHEDULER:New(nil, function()
 
+    alliedSet = SET_CLIENT:FilterCoalitions("blue"):FilterStart()
+    alliedSet:ForEachClient(function(client)
+        
+          
 
 
+      end)
 end
 ,{},300 ,300)
+
+
+
 
 
   

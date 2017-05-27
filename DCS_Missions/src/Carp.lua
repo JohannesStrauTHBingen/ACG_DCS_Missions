4ce0185169c44bdf89ce220d87c03fb776c3567e
Johannes <johannes.a.strauss@th-bingen.de>
2017-05-27 20:12:07 +0200

    Carpa.lua

---
+++ DCS_Missions/src/Carp.lua
@@ -0,0 +1,31 @@
+
+
+kingTigerSpawn = SPAWN:New("KingTiger")
+  :InitLimit(1,10)
+  :SpawnScheduled(900, 0)
+
+tigerSpawn = SPAWN:New("Tiger")
+  :InitLimit(2, 20)
+  :SpawnScheduled(300, 0)
+
+pantherSpawn = SPAWN:New("Panther")
+  :InitLimit(2, 20)
+  :SpawnScheduled(300, 0)
+
+panzerIVSpawn = SPAWN:New("PanzerIV")
+  :InitLimit(5,50)
+  :SpawnScheduled(60, 0)
+
+ShermanSpawn = SPAWN:New("Sherman")
+  :InitLimit(10,50)
+  :SpawnScheduled(60, 0)
+
+FireflySpawn = SPAWN:New("Firefly")
+  :InitLimit(5,25)
+  :SpawnScheduled(60, 0)
+
+CromwellSpawn = SPAWN:New("Cromwell")
+  :InitLimit(5, 25)
+  :SpawnScheduled(60, 0)
+
+  

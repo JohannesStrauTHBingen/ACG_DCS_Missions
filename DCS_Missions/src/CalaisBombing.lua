b5cfed2e1946f88e69b20ede4ede91da7245a031
Johannes <johannes.a.strauss@th-bingen.de>
2017-05-27 11:56:10 +0200

    anfang radar

--- DCS_Missions/src/CalaisBombing.lua
+++ DCS_Missions/src/CalaisBombing.lua
@@ -1,12 +1,20 @@
 
 
 --Spawn a BomberGroup every 30 minutes
-
 bomberSpawn = SPAWN:New( "Bombers" )
-  :SpawnScheduled(1800,0)  
+  :SpawnScheduled(1800,0)
   :OnSpawnGroup(function( SpawnGoup )
-  
-  MESSAGE:New("Bombers are on their way to Calais at " .. SpawnGroup:GetMaxHeight() ..  " m! Protect them." ,200,nil):ToBlue()
-  MESSAGE:New("Möbelwagen detected on their way to Calais " .. SpawnGroup:GetMaxHeight() ..  " m! Pauke Pauke!." ,200,nil):ToRed()
-  
+      MESSAGE:New("Bombers are on their way to ! Protect them." ,200,nil):ToBlue()
+      MESSAGE:New("Möbelwagen detected on their way ! Pauke Pauke!." ,200,nil):ToRed()
+
   end)
+
+radarSchedular = SCHEDULER:New(nil, function()
+
+
+
+end
+,{},300 ,300)
+
+
+  

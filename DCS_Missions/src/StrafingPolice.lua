8e7545895875042c8ff7a40fc39e8e3c70a32b64
Johannes <johannes.a.strauss@th-bingen.de>
2017-06-02 13:19:41 +0200

    debug lines

--- DCS_Missions/src/StrafingPolice.lua
+++ DCS_Missions/src/StrafingPolice.lua
@@ -6,13 +6,20 @@ local clientGroupSetBlue = SET_GROUP:New()
   :FilterCoalitions("blue")
   :FilterStart()
 clientGroupSetBlue:ForEachGroup(function(group)
-  
-  group:HandleEvent(Events.Shot)
 
-  function group:OnEventShot(EventData)
-    local targetGroup = GROUP:Find(EventData.target)
-    if targetGroup:AllOnGround() and targetGroup:IsCompletelyInZone(redAirfield) then
-      group:Destroy()
+
+    function group:OnEventShot(EventData)
+
+      MESSAGE:New("Handling hit",10,nil):ToAll()
+      
+      group:HandleEvent(EVENTS.Shot)
+
+      local targetGroup = GROUP:Find(EventData.target)
+      if targetGroup:AllOnGround() and targetGroup:IsCompletelyInZone(redAirfield) then
+        group:Destroy()
+      end
     end
-  end
+    MESSAGE:New("Shot got handeled",10,nil)
 end)
+
+MESSAGE:New("All lines loaded",10,nil):ToAll()

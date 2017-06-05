2a981fe3cc962c6a3d15adc7c9c8fb8050defd45
Alpenwolf <hokumyounis@yahoo.com>
2017-06-05 20:54:41 +0200

    Battle For Maykop ALPENWOLF
    die sound file function wurde geändert

--- Battle for Maykop ALPENWOLF.lua
+++ Battle for Maykop ALPENWOLF.lua
@@ -19,7 +19,8 @@ MaykopAttackingGroupREDGroupIsAliveChecker , MaykopAttackingGroupREDGroupIsAlive
  
   if maykopOccupied and not MaykopAttackingGroupRED:IsAlive()  then
     maykopOccupied = false
-    function trigger.action.outSound(string computerbeep_11)
+    trigger.action.outSoundForCoalition(coalition.side.BLUE, 'computerbeep_11')
+    trigger.action.outSoundForCoalition(coalition.side.RED, 'computerbeep_11')
     MESSAGE:New("Good job! Maykop is ours boys!",60,nil):Blue()
     MESSAGE:New("The enemy has captured Maykop! We must counterstrike. Maykop MUST be only ours!"):ToRed()
     MaykopDefenceGroupBLUE:Spawn()
@@ -37,7 +38,8 @@ MaykopAttackingGroupBLUEGroupIsAliveChecker , MaykopAttackingGroupBLUEGroupIsAli
  
   if maykopOccupied and not MaykopAttackingGroupBLUE:IsAlive()  then
     maykopOccupied = false
-    function trigger.action.outSound(string computerbeep_11)
+    trigger.action.outSoundForCoalition(coalition.side.BLUE, 'computerbeep_11')
+    trigger.action.outSoundForCoalition(coalition.side.RED, 'computerbeep_11')
     MESSAGE:New("Good job! Maykop is ours boys!",60,nil):ToRed()
     MESSAGE:New("The enemy has captured Maykop! We must counterstrike. Maykop MUST be only ours!"):ToBlue()
     MaykopDefenceGroupRED:Spawn()

5fd928e74024aeffaff2817ec8f37ee7160d52ac
JohannesStrauTHBingen <johannes.a.strauss@th-bingen.de>
2017-06-28 19:19:07 +0200

    InterceptLogik fertig

--- DCS_Missions/src/WW2BomberInterception.lua
+++ DCS_Missions/src/WW2BomberInterception.lua
@@ -1,13 +1,20 @@
-
-local hq = GROUP:FindByName("AlderHorst")
+local hq = GROUP:FindByName("AdlerHorst")
 
 local commandCenter = COMMANDCENTER:New(hq,"AdlerHorst")
   :SetModeWWII()
+  :SetReferenceZones("Azeville")
+  :SetReferenceZones("Les Dunes de Varreville")
+  :SetReferenceZones("Grandcamp les Bains")
+  :SetReferenceZones("Vierville suer Mer")
+  :SetReferenceZones("La Vailee")
+  :SetReferenceZones("Ranchy")
+  :SetReferenceZones("Cateaubrave")
+  :SetReferenceZones("Isigny")
+  
 
-local referenceZones = {"Azeville", "Les Dunes de Varreville", "Grandcamp les Bains", "Vierville suer Mer", "La Vailee", "Ranchy", "Cateaubrave", "Isigny"}
-for referenceZone in referenceZones do
-  commandCenter:SetReferenceZones(referenceZone)
-end
+MESSAGE:New("HQ done!",20,"Debug"):ToAll()
+
+--local referenceZones = {"Azeville", "Les Dunes de Varreville", "Grandcamp les Bains", "Vierville suer Mer", "La Vailee", "Ranchy", "Cateaubrave", "Isigny"}
 
 local jagtwaffe = SET_GROUP:New():FilterCategories("plane"):FilterCoalitions("red"):FilterStart()
 local bombers = SET_GROUP:New():FilterCoalitions("blue"):FilterCategories("plane"):FilterPrefixes("First"):FilterStart()
@@ -17,4 +24,8 @@ local mission = MISSION
   :New(commandCenter,"WestWall","Primary","Destroy incomming Bombers!",coalition.side.RED)
   :AddScoring(jagt)
 
-local interceptTask = TASK_A2A_INTERCEPT:New(mission,jagtwaffe,"Intercept",bombers)  
+MESSAGE:New("Mission done!",20,"Debug"):ToAll()
+
+local interceptTask = TASK_A2A_INTERCEPT:New(mission,jagtwaffe,"Intercept",bombers)
+
+MESSAGE:New("Intercepterscript done!",20,"Debug"):ToAll()

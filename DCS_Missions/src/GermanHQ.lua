7108a765baeffe6719b9d2d4e0849baf998c15cb
Johannes <johannes.a.strauss@th-bingen.de>
2017-06-29 14:37:51 +0200

    ATG fertig

--- DCS_Missions/src/GermanHQ.lua
+++ DCS_Missions/src/GermanHQ.lua
@@ -17,6 +17,7 @@ MESSAGE:New("HQ done!",20,"Debug"):ToAll()
 --local referenceZones = {"Azeville", "Les Dunes de Varreville", "Grandcamp les Bains", "Vierville suer Mer", "La Vailee", "Ranchy", "Cateaubrave", "Isigny"}
 
 local jagtwaffe = SET_GROUP:New():FilterCategories("plane"):FilterCoalitions("red"):FilterStart()
+local gerRecUnits = SET_GROUP:New():FilterCoalitions("red"):FilterCategories("ground"):FilterPrefixes("GerReccon"):FilterStart()
 local bombers = SET_GROUP:New():FilterCoalitions("blue"):FilterCategories("plane"):FilterPrefixes("First"):FilterStart()
 local groundTargets = SET_GROUP:New():FilterCoalitions("blue"):FilterPrefixes("GroundForces"):FilterStart()
 
@@ -26,6 +27,19 @@ local mission = MISSION
   :AddScoring(jagt)
 
 local interceptTask = TASK_A2A_INTERCEPT:New(mission,jagtwaffe,"Intercept",bombers)
-local casTRask = TASK_A2G_BAI:New(mission,jagtwaffe,"Close Air Support", groundTargets)
+--local casTRask = TASK_A2G_BAI:New(mission,jagtwaffe,"Close Air Support", groundTargets)
+
+
+local gerRec = DETECTION_UNITS:New(gerRecUnits)
+local CASTasking = TASK_A2G_DISPATCHER:New(mission,jagtwaffe,gerRec)
+
+function mission:OnAfterMissionGoals( From, Event, To )
+  self:E( { From, Event, To } )
+  local TargetGroup = GROUP:FindByName( "GroundTargetHostShip" )
+  if TargetGroup and not TargetGroup:IsAlive() then
+    mission:GetCommandCenter():MessageToCoalition( "Mission Complete! All targets have been destroyed!" )
+    mission:Complete()
+  end
+end
 
 MESSAGE:New("Intercepterscript done!",20,"Debug"):ToAll()

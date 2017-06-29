25117383333ee09128ddfd9cc80b8b236000cf9e
JohannesStrauTHBingen <johannes.a.strauss@th-bingen.de>
2017-06-29 19:38:53 +0200

    Weiter arbeit geht aber net

--- DCS_Missions/src/GermanHQSchedular.lua
+++ DCS_Missions/src/GermanHQSchedular.lua
@@ -1,12 +1,28 @@
 
-local jagtwaffe = SET_CLIENT:FilterCategories("plane"):FilterCoalitions("red"):FilterStart()
+
+
+local jagtwaffe = SET_CLIENT:New():FilterCategories("plane"):FilterCoalitions("red"):FilterStart()
 local bombers = SET_GROUP:New():FilterCoalitions("blue"):FilterCategories("plane"):FilterPrefixes("First"):FilterStart()
 
-jagtwaffe:ForEachClient(function(client)
-  if(bombers:Count() > 0)then
-    local unitPos = POINT_VEC3:NewFromVec3(client:GetClientGroupUnit():GetPointVec3())
-    local bomber = bombers:GetFirst()
-    local bomberPos = POINT_VEC3:NewFromVec3(bomber:GetPointVec3())
-    MESSAGE:New(POINT_VEC3.ToStringBRA(bomberPos,unitPos, SETTINGS:SetA2A_BRA()),15,"RadarInfo"):ToClient(client)
-  end
-end)
+MESSAGE:New("All Sets Done",10,"Debug"):ToAll()
+
+--UNIT:GetCoordinate()
+--CLIENT:GetClientGroupUnit()
+--COORDINATE:GetBRAText(AngleRadians,Distance,Settings)
+--
+--COORDINATE:ToStringBR(TargetCoordinate,FromCoordinate,Settings)
+
+
+local radarSchedular = SCHEDULER:New(nil,function()
+  jagtwaffe:ForEachClient(function(client)
+    if(bombers:Count() > 0)then
+      local bomber = bombers:GetFirst()
+      local bomberPos = bomber:GetCoordinate()
+      local clientPos = client:GetCoordinate()
+      
+      bomberPos:ToStringBRA(clientPos)
+      --COORDINATE:GetBRAText(COORDINATE:GetAngleRadians(bomberPos:GetVec3()))
+      MESSAGE:New("A private Message just for you" ,15,"RadarInfo"):ToClient(client)
+    end
+  end)
+end,{},0,30)
